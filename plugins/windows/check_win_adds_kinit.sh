abbdce206ba14144bdcee35e7a4a44335c9ad445
virot <virot@virot.com>
2020-11-15 19:55:56 +0100

    changes required

--- plugins/windows/check_win_adds_kinit.sh
+++ plugins/windows/check_win_adds_kinit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while getopts "p:r:s:u:" opt; do
+while getopts "p:r:s:u:v:" opt; do
     case "$opt" in
         p )
             password=$OPTARG
@@ -14,57 +14,66 @@ while getopts "p:r:s:u:" opt; do
         u )
             user=$OPTARG
             ;;
+	v )
+            verbose=$OPTARG
+	    ;;
     esac
 done
 
+    function cleanup {
+        if [ -x $KRB5CCNAME ]; then
+            rm $KRB5CCNAME
+        fi
+        if [ -x $KRB5_CONFIG ]; then
+            rm $KRB5_CONFIG
+        fi
+    }
+    
+    trap cleanup EXIT
+    
+    export KRB5_CONFIG=/tmp/krb5_$RANDOM.conf
+    export KRB5CCNAME=/tmp/krb5_CC_$RANDOM
+
 if [ -z "$user" ] || [ -z "$password" ] || [ -z "$server" ]; then
     echo "UNKNOWN - Missing Argument - check_ms_ad_kinit -u <user> -p <password> -s <server>"
-    return 3
-fi
-
-if [ -z "$realm"]; then
-    realm=${user/*@/}
-fi
-
-function cleanup {
-    if [ -x $KRB5CCNAME ]; then
-        rm $KRB5CCNAME
-    fi
-    if [ -x $KRB5_CONFIG ]; then
-        rm $KRB5_CONFIG
-    fi
-}
-
-trap cleanup EXIT
-
-export KRB5_CONFIG=/tmp/krb5_$RANDOM.conf
-export KRB5CCNAME=/tmp/krb5_CC_$RANDOM
-
-cat > $KRB5_CONFIG << EOF
-[libdefaults]
-  default_realm = $realm
-  dns_lookup_kdc = false
-[realms]
-  AD.VIROT.SE = {
-    kdc = $server
-    admin_server = $server
-  }
-EOF
-
-
-#export KRB5_CONFIG=<(echo $KRB5CONF); KRB5CCNAME=FILE:<(echo bla); kinit $user < <(echo $password) > /dev/null
-result=$(kinit $user < <(echo $password) 2>&1 > /dev/null)
-retvalue=$?
-#kdestroy
-#KRB5_CONFIG=<(echo $KRB5CONF); KRB5CCNAME=FILE:<(echo bla); cat $KRB5_CONFIG
-if [ "$retvalue" -eq "0" ]; then
-    echo "OK - Ticket granted"
-    return 0
-elif [ "$retvalue" -eq "1" ]; then
-    echo "CRITICAL - Failed to get ticket $result"
-    return 2
 else
-    echo "UNKNOWN - kinit returned: $retvalue, $result"
-    return 3
+    if [ -z "$realm"]; then
+        realm=${user/*@/}
+    fi
+    
+    if [ ! -z "$verbose" ] && [ $verbose -gt 0 ]; then
+        echo "REALM: $realm"
+        echo "Server: $server"
+        echo "User: $user"
+        echo "Password: $password"
+    fi
+    
+    
+    cat > $KRB5_CONFIG << EOF
+    [libdefaults]
+      default_realm = $realm
+      dns_lookup_kdc = false
+    [realms]
+      $realm = {
+        kdc = $server
+        admin_server = $server
+      }
+EOF
+    
+    
+    #export KRB5_CONFIG=<(echo $KRB5CONF); KRB5CCNAME=FILE:<(echo bla); kinit $user < <(echo $password) > /dev/null
+    result=$(kinit $user < <(echo $password) 2>&1 > /dev/null)
+    retvalue=$?
+    #kdestroy
+    #KRB5_CONFIG=<(echo $KRB5CONF); KRB5CCNAME=FILE:<(echo bla); cat $KRB5_CONFIG
+    if [ "$retvalue" -eq "0" ]; then
+        echo "OK - Ticket granted"
+        return 0
+    elif [ "$retvalue" -eq "1" ]; then
+        echo "CRITICAL - Failed to get ticket $result"
+        return 2
+    else
+        echo "UNKNOWN - kinit returned: $retvalue, $result"
+        return 3
+    fi
 fi
-                                                                                                                                                                                                                                                                                                                         ~                                                                                                                                                                                                                                                                                                                            ~                                                                                                                                                                                                                                                                                                                            ~                                                                             
--- plugins/windows/check_win_smb.sh
+++ plugins/windows/check_win_smb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while getopts "p:r:s:u:" opt; do
+while getopts "p:s:u:v:" opt; do
     case "$opt" in
         s )
             server=$OPTARG
@@ -11,26 +11,35 @@ while getopts "p:r:s:u:" opt; do
         p )
             password=$OPTARG
             ;;
+        v )
+            verbose=$OPTARG
+            ;;
     esac
 done
-
+echo $server
 if [ -z "$user" ] || [ -z "$password" ] || [ -z "$server" ]; then
     echo "UNKNOWN - Missing Argument - check_ms_lanmanserver -u <user> -p <password> -s <server>"
     return 3
-fi
-
-resultpre=$(smbclient -L $server -U $user < <(echo $password) 2>&1)
-retvalue=$?
-
-result=$(sed -e 's/^Enter [^\n]*//' <<< "$resultpre")
-
-if [ "$retvalue" -eq "0" ]; then
-    echo "OK - SMB connected"
-    return 0
-elif [ "$retvalue" -eq "1" ]; then
-    echo "CRITICAL - failed to connect: $result"
-    return 2
 else
-    echo "UNKNOWN - SMB returned: $retvalue, $result"
-    return 3
+
+    if [ -n "$verbose" ] && [ $verbose -gt 0 ]; then
+        echo "Server: $server"
+        echo "User: $user"
+        echo "Password: $password"
+    fi
+
+    result=$(smbclient -L $server -U $user%$password -m SMB3 -e --option='client min protocol=SMB2' 2>/dev/null)
+    retvalue=$?
+    
+    
+    if [ "$retvalue" -eq "0" ]; then
+        echo "OK - SMB connected"
+        return 0
+    elif [ "$retvalue" -eq "1" ]; then
+        echo "CRITICAL - failed to connect: $result"
+        return 2
+    else
+        echo "UNKNOWN - SMB returned: $retvalue, $result"
+        return 3
+    fi
 fi
